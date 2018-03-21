3ad0e1e4dd1ca6bebd829d216aac22cd0ba0109d
Pablito De Cecco <pablomdececcorios@gmail.com>
2018-03-20 23:44:27 -0300

    firebase and firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
+//------------------------Pages------------------------//
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+//------------------------Local Provider------------------------//
 import { ApiProvider } from '../providers/api/api';
+
+//------------------------Providers------------------------//
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig  = {
+  apiKey: "AIzaSyAhU9S_jk3tRU0pxYft15f7wYa-3yuwcSY",
+  authDomain: "asistencia-63f29.firebaseapp.com",
+  databaseURL: "https://asistencia-63f29.firebaseio.com",
+  projectId: "asistencia-63f29",
+  storageBucket: "asistencia-63f29.appspot.com",
+  messagingSenderId: "1053669090196"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +35,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiProvider } from '../../providers/api/api';
+import { AngularFirestore,AngularFirestoreDocument  } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 //import { MenuPage } from '../menu/menu';
 
 @Component({
@@ -11,17 +13,37 @@ import { ApiProvider } from '../../providers/api/api';
 export class HomePage {
   formLogin: FormGroup;
   mail: string;
-  password: string;
-  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, private ApiProvider: ApiProvider) {
+  password: string;  
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, private ApiProvider: ApiProvider, private db: AngularFirestore) {
     this.formLogin = formBuilder.group({
       mail: [this.mail, Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
       password: [this.password, Validators.compose([Validators.maxLength(30), Validators.required])]
-    });
+    });     
+    
+    
   }
+  
+  registrar(){    
+    this.db.collection("usuarios").add({
+      nombre: "Pablo",
+      apellido: "De Cecco",
+      mail:'pablo.dececco@hotmail.com',
+      password:'722567'
+    })
+      .then( (docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch( (error) => {
+        console.error("Error adding document: ", error);
+      });
+  }
+ 
+  
 
   ingresar() {
     this.ApiProvider.verificarUsuario(this.mail,this.password).then(response=>{
       console.info(response)
+          
     }).catch(error=>{
       console.warn(error)
     })   
